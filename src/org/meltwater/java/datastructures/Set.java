cafa84ade9f1ae9dd1deca835d6a6ef7df991c93
Muyide Ibukun <superibk@gmail.com>
2015-10-09 00:21:06 +0000

    corret set class and add more methods

--- src/org/meltwater/java/datastructures/Set.java
+++ src/org/meltwater/java/datastructures/Set.java
@@ -1,183 +1,71 @@
 package org.meltwater.java.datastructures;
 
-import java.util.NoSuchElementException;
-import java.util.TreeSet;
-
-public class Set<Key extends Comparable<Key>> {
-	private TreeSet<Key> set;
+public class Set<E extends Comparable> {
+	private BetterArray<E> set;
 
 	/**
 	 * Initialises an empty set.
 	 */
 	public Set() {
-		set = new TreeSet<Key>();
+		set = new BetterArray<E>();
 	}
 
-	/**
-	 * Adds the key to this set (if it is not already present).
-	 * 
-	 * @param key
-	 *            the key to add
-	 * @throws NullPointerException
-	 *             if key is null
-	 */
-	public void add(Key key) {
-		if (key == null)
+	public void add(E element) {
+		if (element == null)
 			throw new NullPointerException("called add() with a null key");
-		set.add(key);
+		set.append(element);
 	}
 
-	/**
-	 * Returns true if this set contains the given key.
-	 * 
-	 * @param key
-	 *            the key
-	 * @return true if this set contains key and false otherwise
-	 * @throws NullPointerException
-	 *             if key is null
-	 */
-	public boolean contains(Key key) {
-		if (key == null)
+	public boolean contains(E element) {
+		if (element == null)
 			throw new NullPointerException("called contains() with a null key");
-		return set.contains(key);
+		return set.contains(element);
 	}
 
-	/**
-	 * Removes the key from this set (if the key is present).
-	 * 
-	 * @param key
-	 *            the key
-	 * @throws NullPointerException
-	 *             if key is null
-	 */
-	public void delete(Key key) {
-		if (key == null)
+	public void delete(E element) {
+		if (element == null)
 			throw new NullPointerException("called delete() with a null key");
-		set.remove(key);
+		set.remove(element);
 	}
 
-	/**
-	 * Returns the number of keys in this set.
-	 * 
-	 * @return the number of keys in this set
-	 */
 	public int size() {
 		return set.size();
 	}
 
-	/**
-	 * Returns true if this set is empty.
-	 * 
-	 * @return true if this set is empty, and false otherwise
-	 */
 	public boolean isEmpty() {
 		return size() == 0;
 	}
 
-	/**
-	 * Returns the largest key in this set.
-	 * 
-	 * @return the largest key in this set
-	 * @throws NoSuchElementException
-	 *             if this set is empty
-	 */
-	public Key max() {
+	public E max() {
+		E maxElement = null;
 		if (isEmpty())
-			throw new NoSuchElementException("called max() with empty set");
-		return set.last();
-	}
-
-	/**
-	 * Returns the smallest key in this set.
-	 * 
-	 * @return the smallest key in this set
-	 * @throws NoSuchElementException
-	 *             if this set is empty
-	 */
-	public Key min() {
-		if (isEmpty())
-			throw new NoSuchElementException("called min() with empty set");
-		return set.first();
-	}
-
-	/**
-	 * Returns the smallest key in this set greater than or equal to key.
-	 * 
-	 * @param key
-	 *            the key
-	 * @return the smallest key in this set greater than or equal to key
-	 * @throws NoSuchElementException
-	 *             if there is no such key
-	 * @throws NullPointerException
-	 *             if key is null
-	 */
-	public Key ceiling(Key key) {
-		if (key == null)
-			throw new NullPointerException("called ceiling() with a null key");
-		Key k = set.ceiling(key);
-		if (k == null)
-			throw new NoSuchElementException("all keys are less than " + key);
-		return k;
-	}
-
-	/**
-	 * Returns the largest key in this set less than or equal to key.
-	 * 
-	 * @param key
-	 *            the key
-	 * @return the largest key in this set table less than or equal to key
-	 * @throws NoSuchElementException
-	 *             if there is no such key
-	 * @throws NullPointerException
-	 *             if key is null
-	 */
-	public Key floor(Key key) {
-		if (key == null)
-			throw new NullPointerException("called floor() with a null key");
-		Key k = set.floor(key);
-		if (k == null)
-			throw new NoSuchElementException("all keys are greater than " + key);
-		return k;
-	}
-
-	
-	/**
-	 * Returns the intersection of this set and that set.
-	 * 
-	 * @param that
-	 *            the other set
-	 * @return the intersection of this set and that set
-	 * @throws NullPointerException
-	 *             if that is null
-	 */
-	public Set<Key> intersects(Set<Key> that) {
-		if (that == null)
-			throw new NullPointerException("called intersects() with a null argument");
-		Set<Key> c = new Set<Key>();
-		if (this.size() < that.size()) {
-			for (Key x : this) {
-				if (that.contains(x))
-					c.add(x);
-			}
-		} else {
-			for (Key x : that) {
-				if (this.contains(x))
-					c.add(x);
-			}
+			throw new RuntimeException("called max() with empty set");
+		for (int k = 0; k <= size() - 1; k++) {
+			if (set.get(k).compareTo((E) maxElement) >= 0)
+				maxElement = set.get(k);
 		}
-		return c;
+		return maxElement;
+
 	}
 
-	/**
-	 * This operation is not supported because sets are mutable.
-	 * 
-	 * @return does not return a value
-	 * @throws UnsupportedOperationException
-	 *             if called
-	 */
-	@Override
-	public int hashCode() {
-		throw new UnsupportedOperationException("hashCode() is not supported because sets are mutable");
+	public E min() {
+		E minElement = null;
+		if (isEmpty())
+			throw new RuntimeException("called min() with empty set");
+		for (int k = 0; k <= size() - 1; k++) {
+			if (set.get(k).compareTo((E) minElement) <= 0)
+				minElement = set.get(k);
+		}
+		return minElement;
+	}
+
+	public String toString() {
+		StringBuilder returnedString = new StringBuilder();
+		for (int i = 0; i <= size() - 1; i++) {
+			returnedString.append(set.get(i).toString() + ", ");
+		}
+		return returnedString.toString();
+
 	}
 
 }
